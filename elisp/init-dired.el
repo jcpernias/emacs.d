e8a9ca102684fc47df30995a393ac3dab9ced469
José C. Pernías <jcpernias@gmail.com>
2023-09-11 10:23:00 +0200

    Change dired configuration

--- elisp/init-dired.el
+++ elisp/init-dired.el
@@ -1,27 +1,34 @@
 ;; init-dired.el -*- lexical-binding: t -*-
 
 ;; case insensitive sort in dired
-(require 'ls-lisp)
-(setq ls-lisp-use-insert-directory-program nil)
-(setq ls-lisp-ignore-case 't)
-(setq ls-lisp-use-string-collate nil)
+;; (require 'ls-lisp)
+;; (setq ls-lisp-use-insert-directory-program nil)
+;; (setq ls-lisp-ignore-case 't)
+;; (setq ls-lisp-use-string-collate nil)
 
 
-;; Dired-x
-(add-hook 'dired-load-hook
-          (lambda ()
-            (load "dired-x")
-            ;; Set dired-x global variables here.  For example:
-            ;; (setq dired-guess-shell-gnutar "gtar")
-            (setq dired-x-hands-off-my-keys nil)
-            (setq dired-omit-files "^\\.?#\\|^\\.$\\|^\\.[^.]")
-            (when (eq system-type 'darwin)
-              (setq dired-guess-shell-alist-user '(("\\.pdf\\'" "open -a Skim"))))))
+(let (gls (executable-find "gls"))
+  (when gls (setq insert-directory-program gls)))
+
+(use-package diredfl
+  :hook (dired-mode . diredfl-global-mode))
+
+(with-eval-after-load 'dired
+  (require 'dired-x)
+  ;; Set dired-x global variables here.  For example:
+  (setq dired-x-hands-off-my-keys nil)
+  (setq dired-omit-files "^\\.?#\\|^\\.$\\|^\\.[^.]")
+  (when (eq system-type 'darwin)
+    (setq dired-guess-shell-alist-user '(("\\.pdf\\'" "open -a Skim")))))
+
 
 (add-hook 'dired-mode-hook
           (lambda ()
-            ;; Set dired-x buffer-local variables here.
-            (dired-omit-mode 1)))
+            ;; Set dired-x buffer-local variables here.  For example:
+            (dired-omit-mode 1)
+            ;; Disable printing from dired
+            (keymap-unset dired-mode-map "P")))
+
 
 (autoload 'dired-jump "dired-x"
   "Jump to Dired buffer corresponding to current buffer." t)
@@ -33,9 +40,9 @@
 (keymap-global-set "C-x 4 C-j" 'dired-jump-other-window)
 
 ;; Disable printig from dired
-(add-hook 'dired-mode-hook
-          (lambda()
-            (keymap-unset dired-mode-map "P")))
+;; (add-hook 'dired-mode-hook
+;;           (lambda()
+;;             (keymap-unset dired-mode-map "P")))
 
 
 (provide 'init-dired)
