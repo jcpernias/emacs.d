128ef3daf3878210bedcaf036c38e0d068bedf64
José C. Pernías <jcpernias@gmail.com>
2023-09-12 16:38:37 +0200

    Turn on flyspell on all text buffers

--- elisp/init-spelling.el
+++ elisp/init-spelling.el
@@ -1,4 +1,4 @@
-;; init-spelling -*- lexical-binding: t -*-
+;; init-spelling.el -*- lexical-binding: t -*-
 
 ;; Use aspell
 (setq ispell-program-name "aspell")
@@ -6,13 +6,15 @@
 (when (executable-find ispell-program-name)
   ;; Add spell-checking in comments for all programming language modes
   (add-hook 'prog-mode-hook 'flyspell-prog-mode)
+  (add-hook 'text-mode-hook 'flyspell-mode))
 
-  (with-eval-after-load 'flyspell
-    ;; (add-to-list 'flyspell-prog-text-faces 'nxml-text-face)
-    ;; (keymap-unset flyspell-mode-map "C-;")
-    (keymap-unset flyspell-mode-map "C-.")
-    (keymap-set flyspell-mode-map "C-+" 'flyspell-correct-at-point)
-    (keymap-set flyspell-mouse-map "<down-mouse-3>" 'flyspell-correct-word)
-    (keymap-unset flyspell-mouse-map "<mouse-3>")))
+(with-eval-after-load 'flyspell
+  (setq flyspell-issue-message-flag nil)
+  ;; (add-to-list 'flyspell-prog-text-faces 'nxml-text-face)
+  ;; (keymap-unset flyspell-mode-map "C-;")
+  (keymap-unset flyspell-mode-map "C-.")
+  (keymap-set flyspell-mode-map "C-+" 'flyspell-correct-at-point)
+  (keymap-set flyspell-mouse-map "<down-mouse-3>" 'flyspell-correct-word)
+  (keymap-unset flyspell-mouse-map "<mouse-3>"))
 
 (provide 'init-spelling)
