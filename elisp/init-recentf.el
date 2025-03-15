cd4fecd0a5797cbde7f631e475409c577236273b
José C. Pernías <jcpernias@gmail.com>
2025-03-15 17:44:19 +0100

    Update recentf configuration

--- elisp/init-recentf.el
+++ elisp/init-recentf.el
@@ -1,10 +1,12 @@
 ;;; init-recentf.el -*- lexical-binding: t -*-
 
-(add-hook 'after-init-hook 'recentf-mode)
-
-(setq-default
- recentf-max-saved-items 1000
- recentf-exclude `("/tmp/" "/ssh:" ,(concat package-user-dir "/.*-autoloads\\.el\\'")))
+(use-package recentf
+      :bind ("C-x C-r" . recentf-open)
+      :config
+      (setq recentf-max-menu-items 20
+            recentf-max-saved-items 500
+       )
+      :hook (after-init . recentf-mode))
 
 
 (provide 'init-recentf)
